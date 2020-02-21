84f12f2d3b7ee27e5a64b9afa45a3f0a40979792
Echeverrias <juanan.echeve@gmail.com>
2020-02-21 15:51:16 +0100

    Cambios en CrawlProcess

--- src/task/tasks.py
+++ src/task/tasks.py
@@ -15,13 +15,15 @@ from django.utils import timezone
 from django.db.models import Q
 from .models import Task
 from job.models import Job
-from utilities import save_error, Lock, write_in_a_file
+from utilities import save_error, write_in_a_file
 import time
 from task.models import Task
 from celery import Celery, shared_task, current_app
 from celery.schedules import crontab
 from django.db.utils import InterfaceError
 from background_task import background
+import os, signal
+
 
 
 app = Celery()
@@ -50,8 +52,10 @@ class CrawlProcess():
             self.process = None
             self.q = None
             self.qitems = None
+            self.qis_scrapping = None
             self._count = 0
             self._items = []
+            self._is_resetting = False
             self._init_signals()
 
 
@@ -66,9 +70,11 @@ class CrawlProcess():
         if id:
             try:
                 task = Task.objects.get(id=id)
-            except InterfaceError:
+            except Exception as e: #InterfaceError:
+                print(f'_get_task -> Error: {e}')
                 db.connection.close()
                 task = Task.objects.get(id=id)
+                print(f'_get_task -> task.id: {task.id}')
             return task
         else:
             return None
@@ -76,8 +82,8 @@ class CrawlProcess():
     def _save_task(self, task):
         try:
             task.save()
-        except InterfaceError:
-            db.connection.close()
+        except Exception as e:  # InterfaceError:
+            print(f'_get_task -> Error: {e}')
             task.save()
         return task
 
@@ -85,7 +91,7 @@ class CrawlProcess():
         print(f'{multiprocessing.current_process().name}: *!!OPEN')
 
         write_in_a_file('CrawlerProcess.signal.open', {'args': args, 'kwargs': kwargs, 'process': self.process}, 'task.txt')
-        self.count = 0
+        self._count = 0
         try:
             t = self._get_task(self._id_task)
             t.name = str(self.process.pid)
@@ -98,11 +104,11 @@ class CrawlProcess():
 
 
     def _spider_closed(self, spider, reason):
-        print(f'{multiprocessing.current_process().name}: *!!CLOSE')
+        print(f'{multiprocessing.current_process().name}: *!!CLOSING SPIDER')
         write_in_a_file('CrawlerProcess.signal.close', {'reason': reason}, 'task.txt')
         t = self._get_task(self._id_task)
         d = datetime.today()
-        t.description = f'spider closed with count: {CrawlProcess.count} at {str(d)}'
+        t.description = f'spider closed with count: {self._count} at {str(d)}'
         t.result = self._count
         self._save_task(t)
 
@@ -118,9 +124,10 @@ class CrawlProcess():
         write_in_a_file('CrawlerProcess.signal.scraped_item', {'response': response, 'item': item, 'count': n}, 'task.txt')
         try:
             self.q.get_nowait()
-            self.q.put_nowait(n)
         except:
-            self.q.put_nowait(n)
+            pass
+        finally:
+            self.q.put(n)
 
     def _engine_stopped(self, *args, **kwargs):
         write_in_a_file('CrawlerProcess.signal.stopped', {'args': args, 'kwargs': kwargs}, 'task.txt')
@@ -128,8 +135,41 @@ class CrawlProcess():
     def _spider_error(self, *args, **kwargs):
         write_in_a_file('CrawlerProcess.signal.error', {'args': args, 'kwargs': kwargs}, 'task.txt')
 
-    def _crawl(self):
+    def _empty_queue(self, q):
+        write_in_a_file(f'CrawlProcess._clear_queque:', {}, 'tasks.txt')
+        print(f'CrawlProcess._clear_queque:')
+        while True:
+            try:
+                self.q.get(block=False)
+            except Exception as e:
+                print(f'CrawlProcess._clear_queque - Error: {e}')
+                print(f'CrawlProcess._clear_queque - queue.qsize: {q.qsize()}')
+                write_in_a_file(f'CrawlProcess._clear_queque: error - {e}', {}, 'tasks.txt')
+                write_in_a_file(f'CrawlProcess._clear_queque: error - queue.qsize: {q.qsize()}', {}, 'tasks.txt')
+                break
+        print(f'CrawlProcess._clear_queque: queue empty?')
+        write_in_a_file(f'CrawlProcess._clear_queque: queue empty?', {}, 'tasks.txt')
+
+
+    def _close_queue(self, q):
+        print(f'CrawlProcess._close_queque:')
+        try:
+            write_in_a_file(f'CrawlProcess._clear_queque: close...', {}, 'tasks.txt')
+            q.close()
+            q.join_thread()
+            write_in_a_file(f'CrawlProcess._clear_queque: queue closed', {}, 'tasks.txt')
+        except Exception as e:
+            print(f'CrawlProcess._close_queque - Error: {e}')
+            write_in_a_file(f'CrawlProcess._clear_queque: close -> {e}', {}, 'tasks.txt')
+
+
+    def _empty_and_close_queue(self, q):
+        self._empty_queue(q)
+        self._close_queue(q)
+
+    def _crawl(self, qis_running):
         write_in_a_file('CrawlerProcess.signal.error', {'signals': dir(signals)}, 't.txt')
+        qis_running.put('running')
         crawler = CrawlerProcess(get_project_settings())
         crawler.crawl(InfoempleoSpider)
         # To prevent the infamous error: django.db.utils.InterfaceError: (0, '')
@@ -137,31 +177,40 @@ class CrawlProcess():
         crawler.start()
         write_in_a_file('CrawlProcess.start: process started', {}, 'debug.txt')
         crawler.join()
-        write_in_a_file('CrawlProcess.crawl: process.join', {}, 'task.txt')
-        write_in_a_file('CrawlProcess.crawl: process.join', {}, 'spider.txt')
+        write_in_a_file('CrawlProcess.crawl: process joined', {}, 'task.txt')
+        write_in_a_file('CrawlProcess.crawl: process joined', {}, 'tasks.txt')
+        write_in_a_file('CrawlProcess.crawl: process joined', {}, 'spider.txt')
         print('Crawler Process has Finished!!!!!')
+        write_in_a_file(f'Crawler Process - before: qis_running.qsize: {qis_running.qsize()}', {}, 'tasks.txt')
+        try:
+            qis_running.get()
+        except Exception as e:
+            write_in_a_file(f'Crawler Process - error in qis_running.get: {e}', {}, 'tasks.txt')
+        write_in_a_file(f'Crawler Process - after: qis_running.qsize: {qis_running.qsize()}', {}, 'tasks.txt')
+        write_in_a_file('===========================================================================================', {}, 'tasks.txt')
+        print();print()
 
 
-    def _clear_queue(self):
-        while not self.q.empty():
-            try:
-                self.q.get_nowait()
-            except:
-                pass
-        while not self.qitems.empty():
-            try:
-                self.qitems.get_nowait()
-            except:
-                pass
+    def _empty_and_close_queues(self):
+        write_in_a_file(f'CrawlProcess._clear_queques: q', {}, 'tasks.txt')
+        self._empty_and_close_queue(self.q)
+        write_in_a_file(f'CrawlProcess._clear_queques: qitems', {}, 'tasks.txt')
+        self._empty_and_close_queue(self.qitems)
+        write_in_a_file(f'CrawlProcess._clear_queques: qis_scrapping', {}, 'tasks.txt')
+        self._empty_and_close_queue(self.qis_scrapping)
+        write_in_a_file(f'CrawlProcess._clear_queques: all queues have been celaned', {}, 'tasks.txt')
+
 
 
     def _init_process(self, user):
         print(f'CrawlerProcess.init_process')
         self.q = Queue()
         self.qitems = Queue()
-        self.q.put_nowait(0)
+        # If qis_scrapping has size > 0 this means the process is scrapping
+        self.qis_scrapping = Queue()
+        self.q.put(0)
         self.qresult = Queue()
-        self.process = Process(target=self._crawl, args=())
+        self.process = Process(target=self._crawl, args=(self.qis_scrapping,))
         task = Task.objects.create(user=user, state=Task.STATE_PENDING, type=Task.TYPE_CRAWLER)
         self._id_task = task.pk
 
@@ -172,38 +221,66 @@ class CrawlProcess():
         self.process.start()
         task = self._get_task(self._id_task)
         task.pid = self.process.pid
-        write_in_a_file('CrawlProcess._start_process: process started', {'pid': self.process.pid}, 'debug.txt')
+        write_in_a_file('CrawlProcess._start_process: process started', {'pid': self.process.pid}, 'tasks.txt')
         task.state = Task.STATE_RUNNING
         self._save_task(task)
 
 
     def _reset_process(self, state=Task.STATE_FINISHED):
+        self._is_resetting = True
         print(f'CrawlerProcess._reset_process({state})')
+        number_of_processed_items  = self.qitems.qsize()
         try:
+
+
+            self._empty_and_close_queues()
+            print("CrawlerProcess._reset_process - queues closed")
             self.process.terminate()
-            write_in_a_file('_reset_process terminated (from stop)', {'is_running': self.process.is_alive()}, 'debug.txt')
+            print('._reset_process 1')
+            write_in_a_file('_reset_process terminated (from stop)', {'is_running': self.process.is_alive()}, 'tasks.txt')
+
             task = self._get_task(self._id_task)
+            print('._reset_process 2')
             task.state = state
             self._save_task(task)
-            self.process.join()  # ! IMPORTANT after .terminate -> .join
+            print('._reset_process 3')
+            write_in_a_file('_reset_process before join (from stop)', {},'tasks.txt')
+            self.process.join(120)  # ! IMPORTANT after .terminate -> .join
+            try:
+                os.kill(self.process.pid, signal.SIGTERM)
+            except Exception as e:
+                print(f'_reset_process - Error trying to kill the process {self.process.pip}: {e}')
+                write_in_a_file(f'_reset_process - Error trying to kill the process {self.process.pip}', {}, 'tasks.txt')
+                pass
+            write_in_a_file('_reset_process after join (from stop)', {}, 'tasks.txt')
             try:
                 task = self._get_task(self._id_task)
-                task.result = self.qitems().qsize()
+                print('._reset_process 4')
+                task.result = number_of_processed_items
                 self._save_task(task)
+                print('._reset_process 5')
             except Exception as  e:
                 print(e)
-            write_in_a_file('_reset_process joinned (from stop)', {'is_running': self.process.is_alive()}, 'debug.txt')
+            write_in_a_file('_reset_process joinned (from stop)', {'is_running': self.process.is_alive()}, 'tasks.txt')
         except Exception as e:
             print(e)
-        self._clear_queue()
+        finally:
+            self.process = None
+            self._is_resetting = False
+
+        self._count = 0
 
 
     def _update_process(self):
-        print('CrawlerProcess._update_process')
+        print(f'CrawlerProcess._update_process -> is_resetting: {self._is_resetting}')
         print(f'process is alive: {self.process and self.process.is_alive()}')
+        write_in_a_file(f'__update_process - process == {self.process.is_alive() if self.process else None}', {}, 'tasks.txt')
         # If the process has finished
-        if self.process and not self.process.is_alive():
-            self._reset_process()
+        try:
+            if not self.process.is_alive() and not self._is_resetting:
+                self._reset_process()
+        except:
+            pass
 
 
     def start(self, user=None, **kwargs):
@@ -216,47 +293,71 @@ class CrawlProcess():
         :param kwargs:
         :return:
         """
-        print(f'self.q.empty(): {self.q and self.q.empty()}')
         print(f'self.q.qsize(): {self.q and self.q.qsize()}')
 
         if not self.is_scrapping():
             task = self._get_task(self._id_task)
-            if task and (task.state == Task.STATE_RUNNING): # The process has finished and we have to update the state
+            if task and (task.state == Task.STATE_RUNNING) and (not self._is_resetting): # The process has finished and we have to update the state
                 self._reset_process()
             self._init_process(user)
             self._start_process()
 
-    def stop(self):
-        print(f'CrawleProcess.stop')
-        self._reset_process(Task.STATE_INCOMPLETE)
+    def _stop(self, state=Task.STATE_INCOMPLETE):
+        print(f'CrawleProcess.stop -> is_resetting: {self._is_resetting}')
+        if not self._is_resetting:
+            self._reset_process(state)
+
 
 
     def get_actual_task(self):
+        print(f'CrawleProcess.get_actual_task')
         self._update_process()
         return self._get_task(self._id_task)
 
 
     def get_latest_task(self):
+        print("get_latest_task")
         last_task = Task.objects.get_latest_crawler_task()
+        print(f"last_task: {last_task}")
         actual_task = self._get_task(self._id_task)
+        print(f"actual_task: {actual_task}")
         # If the latest task from de db has state equal STATE_RUNNING and not is the actual task will be an incomplete task...
         #... and would have to update its state
         is_an_incomplete_task = (
                 last_task and
                 last_task.state == Task.STATE_RUNNING and
-                (not actual_task or actual_task.pk != last_task.pk)
+                (not actual_task or (actual_task.pk != last_task.pk))
         )
         if is_an_incomplete_task:
+            print('last_task is an incomplete task')
             last_task.state = Task.STATE_INCOMPLETE
-            self._save_task(last_task.id)
+            self._save_task(last_task)
         return last_task
 
 
     def is_scrapping(self):
-        print(CrawlProcess.is_scrapping)
-        if self.process:
-            return self.process.is_alive()
+        print('CrawlProcess.is_scrapping')
+        # ñapa
+        if self.qis_scrapping and self.process:
+            __d = {'qis_scrapping.qsize': self.qis_scrapping.qsize(), 'process.is_alive': self.process.is_alive()}
         else:
+            __d = {}
+        write_in_a_file(f'CrawlProcess.is_scrapping', __d, 'tasks.txt')
+        print(f'is_scraping - {__d}')
+        if self.process:
+            write_in_a_file(f'is_scraping - process != None', __d,'tasks.txt')
+            print(f'CrawlProcess.is_scrapping - process.is_alive?:{self.process.is_alive()}')
+            if self.qis_scrapping.qsize() > 0:
+                write_in_a_file(f'is_scraping - there is something in qis_scrapping ({self.qis_scrapping.qsize()}) -> True - process.is_alive -> {self.process.is_alive()}', {}, 'tasks.txt')
+                # return self.process.is_alive()
+                return True
+            else:
+                write_in_a_file(f'is_scraping - there is nothing in qis_scrapping -> False', __d, 'tasks.txt')
+                print('The process is scrapping no more')
+                self._stop(state=Task.STATE_FINISHED)
+                return False
+        else:
+            write_in_a_file('is_scraping - process == None -> False', {}, 'tasks.txt')
             return False
 
 
@@ -271,7 +372,7 @@ class CrawlProcess():
 
     def get_scraped_items_number(self):
         print()
-        print('!!!! CrawlProcess.get_scraped_items_number');print();
+        print('!!!! CrawlProcess.get_scraped_items_number');print()
         count = self._count
         try:
             print(self.q)
@@ -283,37 +384,10 @@ class CrawlProcess():
         except Exception as e:
             print(f'get_scraped_items_number')
            # save_error(e, {'count': count})
-        #return count
-        return self.qitems.qsize()
+        # return count
+        # return self.qitems.qsize()
+        return self._count
 
-
-    def get_scraped_items_percentage(self):
-        # Calcula el total con los items scrapeados de la tarea enterior
-        count = self.get_scraped_items_number()
-        task = Task.objects.get_latest_finished_crawler_task()
-        scraped_jobs = Job.objects.registered_or_modified_after(self.init_datetime)
-        write_in_a_file('Job.objects.registered_or_modified_after', {'scraped-jobs': scraped_jobs and scraped_jobs.count()}, 'debug.txt')
-        if task:
-            old_result = task.result or 20000
-        else:
-            old_result = 20000
-
-        if count < old_result:
-            total = old_result
-        else:
-            total = count
-
-        db_count = self._get_scraped_jobs().count()
-
-        try:
-            percentage = round(db_count/total, 2)
-        except:
-            percentage = 0
-
-        if  percentage >= 0.95 and self.is_scrapping():
-            percentage = 0.95
-
-        return percentage
 
 
 
@@ -337,10 +411,24 @@ def run_crawler():
        print(e)
 
 
-@background(schedule=10)
+@background(schedule=1)
+def prueba2():
+    time.sleep(10)
+
+
+@background(schedule=5)
 def bg_run_crawler():
     #https://srv.buysellads.com/ads/click/x/GTND42QNC6BDCKQ7CV7LYKQMCYYIC2QJFTAD4Z3JCWSD42QYCYYIVKQKC6BIKKQIF6AI6K3EHJNCLSIZ?segment=placement:techiediariescom;
-    write_in_a_file('run crawler start', {}, 'bg-task.txt')
+    #### ÑAPA ##########
+    print();print('bg_run_crawler')
+    Job.objects.filter(state=Job.AREA_LEGAL).delete()
+    print(os.getcwd())
+    try:
+        os.remove('parsed urls state.json')
+        #os.remove('bg-task.txt')
+    except: pass
+    ####################
+
     def start():
         try:
             dispatcher.connect(lambda _: print('finish'), signal=signals.spider_closed)#'item_scraped'
@@ -352,13 +440,20 @@ def bg_run_crawler():
             print('reactor...')
             print('run!!!!!')
             reactor.run()
-            write_in_a_file('run crawler end', {}, 'bg-task.txt')
             print('end!!!!!')
         except Exception as e:
            print(e)
 
     p = Process(target=start)
     p.start()
-    print('process started')
+    print();
+    print(f'run crawler start at {str(datetime.now())}');
+    print()
+    write_in_a_file(f'run crawler start at {str(datetime.now())}', {}, 'bg-task.txt')
     p.join()
+    print();
+    print(f'run crawler end at {str(datetime.now())}');
+    print()
+    write_in_a_file(f'run crawler end at {str(datetime.now())}', {}, 'bg-task.txt')
     print('process joined')
+
--- src/task/views.py
+++ src/task/views.py
@@ -21,23 +21,30 @@ def run_crawler_view(request, *args):
     print('run_crawler_view');print();print()
     c = CrawlProcess.get_instance()
     req = ''
+    write_in_a_file('view request - start', {}, 'tasks_view.txt')
+
     if request.GET.get('crawl', None) != None:
+        print('run_crawler_view: crawl request');
+        write_in_a_file('view request - start get request', {}, 'tasks_view.txt')
         user = User.objects.get(username=request.user)
         print(user)
         c.start(user)
+        write_in_a_file('view request - end get request', {}, 'tasks_view.txt')
         print('CrawlProcess started !!!!')
         req = 'crawl'
-    elif request.GET.get('stop', None) != None:
-        print('run_crawler_view: stop request')
-        write_in_a_file('view?stop: process will be stopped', {}, 'debug.txt')
-        c.stop()
-        write_in_a_file('view?stop: process stopped', {},'debug.txt')
-        req = 'stop'
 
+
+    write_in_a_file('view request - continue', {}, 'tasks_view.txt')
+
+    # se tosta aqui!!!!
     is_running = c.is_scrapping()
+
+    write_in_a_file('view request - continue after call is_scrapping', {}, 'tasks_view.txt')
 
     print(f'run_crawler_view: c.is_scrapping: {is_running}')
     last_task = c.get_actual_task() or c.get_latest_task()
+
+    write_in_a_file('view request - continue after call get_actual_task and get_latest_task', {}, 'tasks_view.txt')
     context = {
         'task': last_task,
         'is_running': is_running,
@@ -45,7 +52,10 @@ def run_crawler_view(request, *args):
         'running_state': Task.STATE_RUNNING,
         'ajax': False,
         'req': req,
+        'scraped_items_number': -100,
     }
+
+    write_in_a_file('view request - continue2', {'is_running': is_running, 'context': context}, 'tasks_view.txt')
 
     if is_running:
         context = { **context, **{
@@ -54,33 +64,36 @@ def run_crawler_view(request, *args):
 
         }
 
+    write_in_a_file('view request - continue3', {'is_running': is_running, 'context': context}, 'tasks_view.txt')
+
     if request.is_ajax():
+        write_in_a_file('view request - ajax request', {'is_running': is_running, 'context': context}, 'tasks_view.txt')
         context['ajax'] = True
         print('AJAX request');print();print()
         print(f'context: {context}');print();print()
         if (last_task.state == Task.STATE_RUNNING):
-            print(f'state: {last_task.state}')
+            print(f'ajax request - state: {last_task.state}');
+            print();
+            print()
             template_name = 'task/info_crawler_task.html'
             #return JsonResponse(context)
             return render(request, template_name, context)
             #return render_to_response(template_name, context)
         elif (last_task.state == Task.STATE_PENDING):
-            print(f'state: {last_task.state}')
-            print('ajax request');print();print()
+            print(f'ajax request - state: {last_task.state}');print();print()
             return HttpResponse('not running')
         elif (last_task.state != Task.STATE_RUNNING):
-            print(f'state: {last_task.state}')
+            print(f'ajax request - state: {last_task.state}');print();print()
             template_name = 'task/init_ajax.html'
             #return render_to_response(template_name, context)
             return render(request, template_name, context)
         else:
-            print(f'state: {last_task.state}')
-            print('ajax request');
+            print(f'ajax request - state: {last_task.state}');print();print()
             return HttpResponse('not running')
 
     else:
         print(f'Context: {context}')
-        write_in_a_file('view request', {'is_running': is_running, 'context': context}, 'debug.txt')
+        write_in_a_file('view request - not ajax request end', {'is_running': is_running, 'context': context}, 'tasks_view.txt')
         template_name = 'task/main.html'
         return render(request, template_name, context)
 
