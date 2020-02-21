13b48dd8a2440577afdafc909056a3a0402ba758
Echeverrias <juanan.echeve@gmail.com>
2020-02-21 15:58:46 +0100

    figures.py has been updated and some changes in the templates and css has been added

--- src/chart/figures.py
+++ src/chart/figures.py
@@ -27,44 +27,223 @@ def _get_colors(colors_column_name_and_column_tuple):
         colors =  [COLOR_AREAS[area] for area in colors_column_name_and_column_tuple[1]]
     return colors
 
+def _get_label(aggregation_or_groupby):
+    dlabel = {
+        'area': 'Área',
+        'province': 'Provincia',
+        'mean_salary': 'Salario medio',
+        'vacancies': 'Vacantes',
+        'registered_people': 'Gente registrada',
+        'company-category': 'Compañía (categoría)',
+        'company': 'Compañía',
+        'category': 'Categoría',
+    }
+    default_label = aggregation_or_groupby.capitalize() if aggregation_or_groupby else('_')
+    return  dlabel.get(aggregation_or_groupby, default_label)
+
+def _get_name_filtered(filter):
+    filter_splitted = filter.split('-')
+    filter_name =  filter_splitted[-1] if type(filter_splitted) == list else  filter_splitted
+    dname = {
+        'nacional': 'nacionales',
+        'internacional': 'internacionales',
+    }
+    return dname.get(filter_name, filter_name)
+
+def _get_operator_aggregation_tuples(operators, aggregations):
+    zipped = zip(operators, aggregations)
+    operation_aggregation_tuples = []
+    for o, a in zipped:
+        operation_aggregation_tuples.append(o + '-' + a)
+    return operation_aggregation_tuples
+
+def _get_title(q, groupby, operator, aggregation, compareby=None):
+    operator_aggregation_tuples = _get_operator_aggregation_tuples(operator, aggregation)
+    key = "__".join(q) + "__" + "__".join(groupby) + "__" + "__".join(operator_aggregation_tuples)
+    if compareby:
+        key += "__" + "__".join(compareby)
+    dtitle = {
+        '__area__sum-vacancies': "Vacantes de ofertas por área",
+        '__area__nationality__sum-vacancies__nationality': "Vacantes de ofertas nacionales e internacionales por área",
+        '__area__sum-registered_people': "Gente registrada en ofertas por área",
+        '__area__nationality__sum-registered_people__nationality': "Gente registrada en ofertas nacionales e internacionales por área",
+        '__area__avg-mean_salary': "Salario medio en ofertas nacionales e internacionales por área",
+        'filter-nationality-national__area__sum-vacancies': "Vacantes de ofertas nacionales por área",
+        'filter-nationality-national__area__sum-registered_people': "Gente registrada en ofertas nacionales por área",
+        'filter-nationality-national__province__sum-vacancies': "Vacantes de ofertas nacionales por provincia",
+        'filter-nationality-national__province__sum-registered_people': "Gente registrada en ofertas nacionales por provincia",
+        'filter-nationality-national__area__avg-mean_salary': "Salario medio en ofertas nacionales por área",
+        'filter-nationality-national__province__avg-mean_salary': "Salario medio en ofertas nacionales por provincia",
+        'filter-nationality-international__area__sum-vacancies': "Vacantes de ofertas internacioneales por área",
+        'filter-nationality-international__area__sum-registered_people': "Gente registrada en ofertas internacioneales por área",
+        'filter-nationality-international__area__avg-mean_salary': "Salario medio en ofertas internacionales por área",
+        'filter-nationality-national__area__sum-vacancies__sum-registered_people': "Vacantes y gente registrada en ofertas nacionales por área",
+        'filter-nationality-international__area__sum-vacancies__sum-registered_people': "Vacantes y gente registrada en ofertas internacionales por área",
+        'filter-nationality-national': '?',
+        'filter-available-jobs-in-year': '?',
+        'filter-publication-date-jobs-in-year': '?',
+        'i-filter-available-jobs-in-years-ago': '?',
+        'i-filter-publication-date-jobs-in-years-ago': '?',
+    }
+    try:
+        default_title = (' y '.join([_get_label(agg) for agg in aggregation]).capitalize() +
+                         " de ofertas " +
+                         (_get_name_filtered(q[0]) if q else "") +
+                        " por " +
+                        _get_label(groupby[0]).lower())
+    except:
+        default_title = '_'
+    return dtitle.get(key, default_title)
+
+def _get_column_name_from_request_query(q):
+    splitted = q.split('-')
+    return splitted[-2] if type(q.split) == list else splitted
+
+def _get_value_from_request_query(q):
+    splitted = q.split('-')
+    return splitted[-1] if type(q.split) == list else splitted
+
+def _get_axis_x(df, groupby):
+    return df[groupby].unique()
+
+def _get_axis_y(df, laggregation, lcompareby):
+    """
+    Example for 'if lcompareby:'
+    data.append(df.loc[df['nationality'] == 'nacional', 'mean_salaries'])
+    data.append(df.loc[df['nationality'] == 'internacional', 'mean_salaries'])
+    """
+    print();print(f'_get_axis-y(df, {laggregation}, {lcompareby})')
+    data = []
+    if lcompareby:
+        compareby = lcompareby[0]
+        types = df[compareby].unique()
+        print(f'compareby: {compareby}')
+        print(f'aggregation: {laggregation[0]}')
+        print(f'types: {types}')
+        for t in types:
+            data.append(df.loc[df[compareby] == t, laggregation[0]])
+    else:
+        for aggregation in laggregation:
+            data.append(df[aggregation])
+    return data
+
+
+def _get_legend(laggregation, lcompareby, df=None, location='upper right'):
+    """
+    #  legend = (['nacional', 'internacional'], 'upper right')
+    """
+    types = []
+    if lcompareby:
+        compareby = lcompareby[0]
+        values = df[compareby].unique()
+        for value in values:
+            types.append(value)
+    else:
+        for aggregation in laggregation:
+            types.append(_get_label(aggregation))
+    return (types, location)
+
+def _get_colors_configuration(df, lgroupby, lcompareby):
+    lcol = lcompareby or lgroupby
+    col = lcol[0]
+    return (col, df[col].unique())
+
+def get_buffer_value_from_figure(figure, format='png', dpi=600):
+    print('get_buffer_value_from_figure')
+    buffer = io.BytesIO()
+    # ¡Tarda 8 veces más que utilizando canvas!
+    # figure.savefig(buffer, format=format, dpi=dpi)
+    canvas = FigureCanvasAgg(figure)
+    canvas.print_png(buffer)
+    print('canvas painted')
+    buffer_value = buffer.getvalue()
+    buffer.close()
+    figure.clear()
+    print('pppp')
+    return  buffer_value
+
+def get_spain_map_figure(map_gdf, data_column_name, title):
+    # Control del tamaño de la figura del mapa
+    fig, ax = plt.subplots(1, 1, figsize=(40, 40))
+
+    # Control del encuadre (área geográfica) del mapa
+    ax.axis([-12, 5, 32, 48])
+
+    # Control del título y los ejes
+    ax.set_title(title,
+                 pad=40,
+                 fontdict={'fontsize': 80, 'color': '#4873ab'})
+    #ax.set_xlabel('Longitud')
+    #ax.set_ylabel('Latitud')
+
+    # Añadir la leyenda separada del mapa
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="7%", pad=0.5)
+    cax.tick_params(axis="y", labelsize=40)
+    # Generar y cargar el mapa
+    map_gdf.plot(column=data_column_name, cmap='plasma', ax=ax,
+                  legend=True, cax=cax, zorder=5)
+
+    # Cargar un mapa base con contornos de países
+    ocean_shape = OCEANS_SHAPE_FILE
+    map_oceans = gpd.read_file(ocean_shape)
+    map_oceans.plot(ax=ax, color='#89c0e8', zorder=0)
+    return fig
+
 
 @trace
-def get_bar_figure(x, y, x_label, y_label, title, legend=([], 'upper right'), bar_horizontal=False, colors_column_name_and_column_tuple=('x', None)):
+def get_bars_figure(x, y, x_label, y_label, title, legend=([], 'upper right'), is_bar_horizontal=False, column_name_and_column_values_tuple_for_colors=('x', None)):
     print(f'********************figures.get_bar_figure')
-    fgr = plt.figure(figsize=(16,12))
     try:
         x = x.unique()  # !! unique
     except:
         pass
+    # To make room for the labels (resize)
+    plt.rcParams.update({'figure.autolayout': True}) # better than plt.tight_layout()
+    """
+    fig_size = plt.rcParams["figure.figsize"]
+    fig_size[0] = 6
+    fig_size[1] = 12
+    plt.rcParams["figure.figsize"] = fig_size
+    """
     # Creamos los ejes
     # axes = f.add_axes([0.40, 0.15, 0.63, 0.65])  # [left, bottom, width, height]
     # https://stackoverflow.com/questions/43326680/what-are-the-differences-between-add-axes-and-add-subplot
-    axes = fgr.subplots()  # [left, bottom, width, height]
-    #fgr.subplots_adjust(left=0.4)
-    ind = np.arange(len(x))
-    size = 0.4
+    fig, axes = plt.subplots(figsize=(14,12)) # plt.subplots(figsize=(14,12))
 
+    #fig = plt.figure(figsize=(16,12)) # plt.figure(figsize=(16,12))
+    #axes = fig.subplots()  # [left, bottom, width, height]
+    #fig.subplots_adjust(left=0.4)
+
+    x_labels_position = np.arange(len(x))
+    separation_between_x_labels_index = len(y) #9*len(y)^2#((len(y) + 1)^3)
+
+    ind = x_labels_position * separation_between_x_labels_index
+    coef = len(y) - 1
+    size = 0.8 #len(y)*5
+    print(f'len(y): {len(y)}')
     if len(y) > 1:
         colors = Colors_list[0:len(y)]
-        offset = size/len(y)
+        offset = (size*coef)/2
         for i, y_ in enumerate(y):
-            if bar_horizontal:
+            if is_bar_horizontal:
                 axes.barh(ind + size*i, y_, height=size, color=colors[i])
             else:
                 axes.bar(ind + size*i, y_, width=size, color=colors[i])
         axes.legend(legend[0], loc=legend[1]) # !! set axes after bars
     else:
         offset = 0
-        colors = _get_colors(colors_column_name_and_column_tuple)
+        colors = _get_colors(column_name_and_column_values_tuple_for_colors)
         if not colors:
             colors = Colors_list[0:len(x)]
-        if bar_horizontal:
+        if is_bar_horizontal:
             axes.barh(ind, *y, color=colors, align='center')
 
         else:
             axes.bar(ind, *y, color=colors)
 
-    if bar_horizontal:
+    if is_bar_horizontal:
         axes.set_yticks(ind + offset)
         axes.set_yticklabels(x)
         axes.set_xlabel(y_label, fontsize=18)
@@ -81,29 +260,53 @@ def get_bar_figure(x, y, x_label, y_label, title, legend=([], 'upper right'), ba
     axes.tick_params(axis="x", labelsize=18)
     axes.tick_params(axis="y", labelsize=18)
     axes.set_title(title, fontsize=18)
-    plt.tight_layout() # resize
-    return fgr
+
+    fig.tight_layout()
+    print('????????????????????????????¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿')
+    return fig
 
 
-def get_buffer_value_from_figure(figure, format='png', dpi=600):
-    buffer = io.BytesIO()
 
-    # ¡Tarda 8 veces más que utilizando canvas!
-    #figure.savefig(buffer, format=format, dpi=dpi)
 
-    canvas = FigureCanvasAgg(figure)
-    canvas.print_png(buffer)
+def get_buffer_of_bars_from_df(df, **kwargs):
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    print('get_buffer_of_plot_from_df')
+    x = _get_axis_x(df, kwargs.get('groupby')[0])
+    print(f'x: {x}')
+    y = _get_axis_y(df, kwargs.get('aggregation'), kwargs.get('compareby'))
+    print(f'y: {y}')
+    x_label = _get_label(kwargs.get('groupby')[0])
+    print(f'x_label: {x_label}')
+    y_label = _get_label(kwargs.get('aggregation')[0])
+    print(f'y_label: {y_label}')
+    title = _get_title(**kwargs)
+    print(f'title: {title}')
+    legend = _get_legend(kwargs.get('aggregation'), kwargs.get('compareby'), df)
+    print('legend')
+    colors_column_name_and_column_tuple = _get_colors_configuration(df, kwargs.get('groupby'), kwargs.get('compareby'))
+    print('colors')
+    figure = get_bars_figure(x, y, x_label, y_label, title, legend, True, colors_column_name_and_column_tuple)
 
-    buffer_value = buffer.getvalue()
-    buffer.close()
-    figure.clear()
-    return  buffer_value
-
-@Lock()
-@trace
-def get_buffer_of_plot(x, y, x_label, y_label, title, legend = [], bar_horizontal=False, colors_column_name_and_column_tuple=('x', None)):
-    figure = get_bar_figure(x, y, x_label, y_label, title, legend, bar_horizontal, colors_column_name_and_column_tuple)
     return get_buffer_value_from_figure(figure)
+
+
+def get_buffer_of_spain_map(df, **kwargs):
+    title = _get_title(**kwargs)
+    data_column_name = kwargs.get('aggregation')
+    figure = get_spain_map_figure(df, data_column_name, title)
+    return get_buffer_value_from_figure(figure)
+####################################################################################################################
+
+
+#get_buffer_of_plot(df['area'].unique(), [df['vacancies'], df['registered_people']], 'Áreas', 'Nº', title, legend, True)
+#get_buffer_of_plot(df['area'].unique(), [df['vacancies'], df['registered_people']], 'Áreas', 'Nº', title, legend, True)
+def get_buffer_of_plot(x, y, x_label, y_label, title, legend = [], is_bar_horizontal=False, colors_column_name_and_column_tuple=('x', None)):
+    figure = get_bars_figure(x, y, x_label, y_label, title, legend, is_bar_horizontal, colors_column_name_and_column_tuple)
+    return get_buffer_value_from_figure(figure)
+
+
+
+
 
 @Lock()
 @trace
--- src/chart/views.py
+++ src/chart/views.py
@@ -5,16 +5,14 @@ from django.db.models import Sum
 from utilities import trace
 from job.models import Job
 from .dataframes import (
-    get_vacancies_per_area_df,
-    get_registered_people_per_area_df,
-    get_vacancies_and_registered_people_per_area_df,
-    get_salaries_per_area_df,
-    get_vacancies_per_province_gdf,
-    get_registered_people_per_province_gdf,
-    get_salary_per_province_gdf,
-    get_companies_with_more_vacancies_df,
+    get_initial_queryset,
+    get_df,
+    merge_spain_gdf_with_df,
+
 )
 from .figures import (
+    get_buffer_of_bars_from_df,
+    get_buffer_of_spain_map,
     get_buffer_of_plot,
     get_spain_map_buffer,
     save_figure_as_image,
@@ -25,7 +23,7 @@ import threading
 from datetime import date
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
+POSSIBLE_REQUEST_QUERIES = ['q', 'groupby', 'operator', 'aggregation', 'compareby']
 
 def _get_images_dir():
     return os.path.join(f'static/img/chart/{date.today().month}')
@@ -37,207 +35,39 @@ def _get_response_from_buffer_value(buffer_value, content_type):
     return response
 
 
-@trace
-def _get_vacancies_or_registered_people_per_area_buffer(vacancies_or_registered_people, show_national_vacancies=True, show_international_vacancies=True):
-    if vacancies_or_registered_people == 'vacancies':
-        df = get_vacancies_per_area_df(show_national_vacancies, show_international_vacancies)
-        print('a1', df.columns) #%
-        sub = "Vacantes"
+
+
+#######################################################################################################
+
+def _get_request_parameters(request):
+    parameters = dict(request.GET)
+    for q_key in POSSIBLE_REQUEST_QUERIES:
+        parameters.setdefault(q_key, [])
+    print(f'parameters: {parameters}')
+    return parameters
+
+
+def chart_view(request, model, desc):
+    print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    print(f'plot_view:plot/{model}/{desc}')
+    parameters = _get_request_parameters(request)
+    df = get_df(model, desc, **parameters)
+    print(df)
+    print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    lgroupby = parameters.get('groupby')
+    groupby = lgroupby and lgroupby[0]
+    if groupby == 'company':
+        df = df.sort_values(parameters.get('aggregation'), ascending=False)[0:15]
+       # parameters['groupby'] = [i if i != 'company' else 'name' for i in parameters['groupby']]
+    elif groupby == 'province':
+        print('/////////////////////////////////////////////////////////////////')
+        df = merge_spain_gdf_with_df(df, 'province')
+    if groupby == 'province':
+        buffer_value = get_buffer_of_spain_map(df,**parameters)
     else:
-        df = get_registered_people_per_area_df(show_national_vacancies, show_international_vacancies)
-        sub = "Gente registrada"
-    data = []
-    legend = None
-    if show_national_vacancies and show_international_vacancies:
-        if sub == "Vacantes":
-            title = f'{sub} nacionales e internacionales por área'
-        elif sub == "Gente registrada":
-            title = f'{sub} por área en vacantes nacionales e internacionales'
-        else:
-            title = f'{sub} nacionales e internacionales por área'
-        legend=(['nacional', 'internacional'], 'upper right')
-        data.append(df.loc[df['nationality']=='nacional', vacancies_or_registered_people])
-        data.append(df.loc[df['nationality']=='internacional', vacancies_or_registered_people])
-    elif show_national_vacancies:
-        if sub == "Vacantes":
-            title = f'{sub} por área en España'
-        elif sub == "Gente registrada":
-            title = f'{sub} en vacantes nacionales'
-        else:
-            title = f'{sub} por área en España'
-        data.append(df[vacancies_or_registered_people])
-    else:
-        if sub == "Vacantes":
-            title = f'{sub} por área en el extranjero'
-        elif sub == "Gente registrada":
-            title = f'{sub} en vacantes internacionales'
-        else:
-            title = f'{sub} por área en el extranjero'
-        data.append(df[vacancies_or_registered_people])
-
-    buffer_value = get_buffer_of_plot(df['area'].unique(), data, 'Áreas', sub, title, legend, True, ('area', df['area'].unique()))
-    return buffer_value
-
-
-def vacancies_or_registered_people_per_area_view(request, column, type_):
-    print('vacancies_or_registered_people_per_area_view')
-    print(f'column: {column}')
-    print(f'type_: {type_}')
-    show_national_vacancies = False
-    show_international_vacancies = False
-    if type_ == 'national':
-        show_national_vacancies = True
-    elif type_ == 'international':
-        show_international_vacancies = True
-    buffer_value = _get_vacancies_or_registered_people_per_area_buffer(column, show_national_vacancies, show_international_vacancies)
+        buffer_value = get_buffer_of_bars_from_df(df, **parameters)
     return _get_response_from_buffer_value(buffer_value, 'image/png')
 
-"""
-def _get_national_and_international_vacancies_or_registered_people_per_area_buffer(vacancies_or_registered_people):
-    if vacancies_or_registered_people == 'vacancies':
-        df = get_national_and_international_vacancies_per_area_df()
-        print('a1', df.columns) #%
-        sub = "Vacantes"
-    else:
-        df = get_national_and_international_registered_people_per_area_df()
-        sub = "Gente registrada"
-    title = f'{sub} por área'
-    legend=(['nacional', 'internacional'], 'upper right' )
-    c1 = df.loc[df['nationality']=='nacional', vacancies_or_registered_people]
-    c2 = df.loc[df['nationality']=='internacional', vacancies_or_registered_people]
-    buffer_value = get_buffer_of_plot(df['area'].unique(), [c1,c2], 'Áreas', sub, title, legend, True)
-    return buffer_value
-"""
-
-def national_and_international_vacancies_or_registered_people_per_area_view(request, column):
-    print('vacancies_or_registered_people_per_area_view')
-    buffer_value = _get_vacancies_or_registered_people_per_area_buffer(column)
-    return _get_response_from_buffer_value(buffer_value, 'image/png')
-
-
-@trace
-def _get_vacancies_and_registered_people_per_area_buffer(national_filter=True, international_filter=True):
-    df = get_vacancies_and_registered_people_per_area_df(national_filter, international_filter)
-    legend = (['Vacantes', 'Gente registrada'], 'upper rigth')
-    if not international_filter:
-        sub = " en España"
-    elif not national_filter:
-        sub = " en el extranjero"
-    title = f'Vacantes y gente registrada {sub}'
-    df.to_csv('df_clean.csv') #ñapa
-    buffer_value = get_buffer_of_plot(df['area'].unique(), [df['vacancies'], df['registered_people']], 'Áreas', 'Nº', title, legend, True)
-    return buffer_value
-
-def vacancies_and_registered_people_per_area_view(request, type_):
-    show_national_vacancies = False
-    show_international_vacancies = False
-    if type_ == 'national':
-        show_national_vacancies = True
-    elif type_ == 'international':
-        show_international_vacancies = True
-    buffer_value = _get_vacancies_and_registered_people_per_area_buffer(show_national_vacancies, show_international_vacancies)
-    return _get_response_from_buffer_value(buffer_value, 'image/png')
-
-
-def _get_salaries_per_area_buffer(show_national_areas=True, show_international_areas=True):
-    df = get_salaries_per_area_df(show_national_areas, show_international_areas)
-    print(df) #%
-    data = []
-    legend = None
-    if not show_international_areas:
-        title = "Salario nacional bruto anual medio"
-        data.append(df['mean_salaries'])
-    elif not show_national_areas:
-        title = "Salario internacional bruto anual medio"
-        data.append(df['mean_salaries'])
-    else:
-        title = "Salario bruto anual medio"
-        data.append(df.loc[df['nationality']=='nacional', 'mean_salaries'])
-        data.append(df.loc[df['nationality']=='internacional', 'mean_salaries'])
-        legend = (['nacional', 'internacional'], 'upper right')
-    buffer_value = get_buffer_of_plot(df['area'].unique(), data, 'Áreas', '€', title, legend, True, ('area', df['area'].unique()))
-    return buffer_value
-
-def salaries_per_area_view(request, type_):
-    show_national_vacancies = False
-    show_international_vacancies = False
-    if type_ == 'national':
-        show_national_vacancies = True
-    elif type_ == 'international':
-        show_international_vacancies = True
-    buffer_value = _get_salaries_per_area_buffer(show_national_vacancies, show_international_vacancies)
-    return _get_response_from_buffer_value(buffer_value, 'image/png')
-
-
-def _get_companies_with_more_vacancies_buffer(show_national_areas=True, show_international_areas=True):
-    df = get_companies_with_more_vacancies_df(show_national_areas, show_international_areas)
-    df = df.sort_values('vacancies', ascending=False)[0:15]
-    print(df) #%
-    data = []
-    legend = None
-    if not show_international_areas:
-        title = "Vacantes nacionales"
-        data.append(df['vacancies'])
-    elif not show_national_areas:
-        title = "Vacantes internacionales"
-        data.append(df['vacancies'])
-    else:
-        title = "Compañías que ofrecen más vacantes"
-        data.append(df.loc[df['nationality']=='nacional', 'vacancies'])
-        data.append(df.loc[df['nationality']=='internacional', 'vacancies'])
-        legend = (['nacional', 'internacional'], 'upper right')
-    buffer_value = get_buffer_of_plot(df['name'].unique(), data, 'Compañías', 'Vacantes', title, legend, True, ('name', df['name'].unique()))
-    return buffer_value
-
-
-def companies_with_more_vacancies_view(request, type_):
-    show_national_vacancies = False
-    show_international_vacancies = False
-    if type_ == 'national':
-        show_national_vacancies = True
-    elif type_ == 'international':
-        show_international_vacancies = True
-    buffer_value = _get_companies_with_more_vacancies_buffer(show_national_vacancies, show_international_vacancies)
-    return _get_response_from_buffer_value(buffer_value, 'image/png')
-
-
-
-def national_and_international_salaries_per_area_view(request):
-    buffer_value = _get_salaries_per_area_buffer(True, True)
-    return _get_response_from_buffer_value(buffer_value, 'image/png')
-
-
-def _get_vacancies_or_registered_people_per_province_buffer(vacancies_or_registered_people):
-
-    if vacancies_or_registered_people == 'vacancies':
-        gdf = get_vacancies_per_province_gdf()
-        sub = "Vacantes"
-    else:
-        gdf = get_registered_people_per_province_gdf()
-        sub = "Gente registrada"
-    title = f'{sub} por provincias'
-
-    #image_base64 = get_image_base64_from_figure(figure)
-   # return image_base64
-    buffer_value = get_spain_map_buffer(gdf, vacancies_or_registered_people, title)
-    return buffer_value
-
-
-def _get_salaries_per_province_buffer():
-    gdf = get_salary_per_province_gdf()
-    buffer_value = get_spain_map_buffer(gdf, 'mean_salaries', "Salario bruto anual")
-    return buffer_value
-
-def vacancies_or_registered_people_per_province_view(request, column):
-    print('vacancies_or_registered_people_per_province_view')
-    print(f'column: {column}')
-    buffer_value = _get_vacancies_or_registered_people_per_province_buffer(column)
-    return _get_response_from_buffer_value(buffer_value, 'image/png')
-
-
-def salaries_per_province_view(request):
-    buffer_value = _get_salaries_per_province_buffer()
-    return _get_response_from_buffer_value(buffer_value, 'image/png')
 
 def _get_statistics_context():
     available_offers = Job.objects.available_offers()
@@ -267,6 +97,7 @@ def main_view(request):
     template_name = 'chart/main.html'
     context = _get_statistics_context()
     print(f'request.GET: {request.GET}')
+    print(f'request.GET.q: {request.GET.get("q")}')
     charts_type = request.GET.get('charts_type')
     print(f'charts_type: {charts_type}')
     get_context = {
@@ -283,14 +114,7 @@ if __name__ != '__main__':
 
     print('queries - pre_load')
     def _preloading():
-        _get_salaries_per_area_buffer(True, False)
-        _get_salaries_per_area_buffer(False, True)
-        _get_vacancies_or_registered_people_per_area_buffer('vacancies', True, False)
-        _get_vacancies_or_registered_people_per_area_buffer('registered_people', True, False)
-        _get_vacancies_or_registered_people_per_area_buffer('registered_people', False, True)
-        _get_vacancies_or_registered_people_per_area_buffer('vacancies', False, True)
-        _get_vacancies_or_registered_people_per_province_buffer('vacancies')
-        _get_vacancies_or_registered_people_per_province_buffer('registered_people')
+     pass
 
     def execute_preloading():
         images_path = os.path.join(os.path.dirname(_get_images_dir()), str(date.today().month))
