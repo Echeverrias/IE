24d9b74b564fefd92df184d5b21f81179ebb7624
Echeverrias <juanan.echeve@gmail.com>
2020-04-28 11:09:33 +0200

    An error has been fixed. If the function validate_email raise an exception the django.core.exceptions.ValidatonError is catched

--- src/account/forms.py
+++ src/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms import ValidationError
+from django.core import exceptions
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.validators import validate_email
@@ -16,7 +17,7 @@ class SignInForm(UserCreationForm):
         email = self.cleaned_data.get('email')
         try:
             validate_email(email)
-        except validate_email.ValidationError:
+        except exceptions.ValidationError as e:
             raise ValidationError('The email is not valid')
         if User.objects.filter(email__exact=email):
             raise ValidationError('The email is associated to other user')
