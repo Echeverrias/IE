959ed3095e3b61dde81edb64fbba2c544255be61
Echeverrias <juanan.echeve@gmail.com>
2020-05-05 14:22:06 +0200

    Some cleaning

--- src/job/urls.py
+++ src/job/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import login_required
-from .views import JobListView, run
+from .views import JobListView
 from .models import Job
 
 urlpatterns = [
     path("list/", JobListView.as_view(), name='list'),
     path("detail/<int:pk>/", login_required(DetailView.as_view(model=Job)), name='detail'),
-    path("run/", run, name='run'), #% ñapa
 ]
 
 """
