2bfa6144969d2a11e6977ce7c9c0dcac4ee8d9e9
Echeverrias <juanan.echeve@gmail.com>
2020-05-21 13:39:10 +0200

    gitignore: added migrations/0*

--- src/chart/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Chart(models.Model):
-    x = models.IntegerField(default=15)
-    y = models.IntegerField(default=25)
--- src/chart/static/chart/chart.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    container = $('main div.container:nth-child(1)');
-    console.log(container);
-    container.addClass('app-container');
-    container.removeClass('container');
-})
