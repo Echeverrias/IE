d896724c2632802f3b8f9c765ade2ee6e72e94e7
Echeverrias <jauan.echeve@gmail.com>
2020-06-02 17:51:52 +0200

    ie_django.url: The serve_static method to serve static files by django in production has been removed

--- src/ie_django/urls.py
+++ src/ie_django/urls.py
@@ -6,21 +6,12 @@ from django.conf import settings
 
 from .views import home_view
 
-def _static_butler(request, path, **kwargs):
-    """
-    Serve static files using the django static files configuration.
-    Passing insecure=True allows serve_static to process, and ignores
-    the DEBUG=False setting
-    """
-    return serve_static(request, path, insecure=True, **kwargs)
-
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('job/', include('job.urls')),
     path('task/', include('task.urls')),
     path('account/', include('account.urls')),
     path('home/', home_view, name='home'),
-    re_path(r'' + settings.STATIC_URL[1:-1] + '/(.+)', _static_butler),
     re_path(r'^$', RedirectView.as_view(url='/home', permanent=True), name='home'),
     path('', RedirectView.as_view(url='/home', permanent=True),  name='home'),
 ]
