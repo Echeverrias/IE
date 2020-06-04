2cb430c3e7ee77a06d14c31d8991ef6e7d6fc809
Echeverrias <jauan.echeve@gmail.com>
2020-06-04 11:05:57 +0200

    account.js: The general error messages are displayed but not the name of the wrong fields

--- src/account/static/account/account.js
+++ src/account/static/account/account.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
-    $('div.alert.alert-danger').attr("hidden", true);
+    alerts = $('div.alert.alert-danger');
+
+    if (alerts.length > 1){ // An error message is displayed
+        for (let i = 1; i < alerts.length; i++) {
+            alerts[i].setAttribute('hidden', true)
+        }
+    }
+    else{
+        alerts.attr("hidden", true);
+    }
 
 })
