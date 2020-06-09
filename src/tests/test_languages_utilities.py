7c6a9ebf57813fb3aac875b5a8bc7a7a66ea8cfa
Echeverrias <jauan.echeve@gmail.com>
2020-06-09 10:24:57 +0200

    test_languages_utilities: _

--- src/tests/test_languages_utilities.py
+++ src/tests/test_languages_utilities.py
@@ -6,4 +6,4 @@ class TestUtilities(TestCase):
     def test_get_languages_and_levels_pairs(self):
         self.assertEqual(lu.get_languages_and_levels_pairs('Se requiere inglés fluido y básico de francés'), [('inglés', 'C1'), ('francés', 'B1')])
         self.assertEqual(lu.get_languages_and_levels_pairs('Se requieren idiomas'), [])
-        self.assertEqual(lu.get_languages_and_levels_pairs('Experiencia en contratación y compras. Castellano Catalán. Nivel avanzado en Microsoft Office. ', [('castellano', 'B2'), ('catalán', 'B2')]))
+        self.assertEqual(lu.get_languages_and_levels_pairs('Experiencia en contratación y compras. Castellano Catalán. Nivel avanzado en Microsoft Office. '), [('castellano', 'B2'), ('catalán', 'B2')])
