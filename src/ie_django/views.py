9dfeef451a063d3e39defdeed0cd3537d693b5a3
Echeverrias <jauan.echeve@gmail.com>
2020-06-29 12:34:58 +0200

    _

--- src/ie_django/views.py
+++ src/ie_django/views.py
@@ -10,5 +10,5 @@ def home_view(request):
 def handler404(request, exception):
     return render(request, '404.html', status=404)
 
-def handler500(request, exception):
+def handler500(request):
     return render(request, '500.html', status=500)
